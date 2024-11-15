1c5a4e198395377c387597f95842cb3fbd8be6e0
dearguru07 <pangalaguruyadav101@gmail.com>
2024-11-15 13:02:16 +0530

    react

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import Props from "./props";
 
 const App = () => {
   return (
+
     <React.Fragment>
-      <Props />
+      <h1>Hello, world!</h1>
+      <p>This is a paragraph within a fragment.</p>
     </React.Fragment>
+
+    // <React.Fragment>
+    //   <Props />
+    // </React.Fragment>
   );
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
