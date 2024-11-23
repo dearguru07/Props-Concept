528ae5c97700b21cdda64e664b7a06feaef1079a
dearguru07 <pangalaguruyadav101@gmail.com>
2024-11-23 10:17:01 +0530

    si

--- src/index.js
+++ src/index.js
@@ -2,49 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Props from "./props";
 import List from "./lists";
+import SideImgs from "./test";
 
-// const App = () => {
-//   return (
-//     <div>
-//       <List/>
-//     </div>
+const App = () => {
+  return (
+    <div>
+      {/* <List/> */}
+      <SideImgs/>
+    </div>
     
-//     // <React.Fragment>
-//     //   <h1>Hello, world!</h1>
-//     //   <p>This is a paragraph within a fragment.</p>
-//     // <List/>
-//     // </React.Fragment>
+    // <React.Fragment>
+    //   <h1>Hello, world!</h1>
+    //   <p>This is a paragraph within a fragment.</p>
+    // <List/>
+    // </React.Fragment>
 
-//     // <React.Fragment>
-//     //   <Props />
-//     // </React.Fragment>
-//   );
-// };
-
-
-// HOC to add logging functionality
-const withLogging = (WrappedComponent) => {
-  return class extends React.Component {
-    componentDidMount() {
-      console.log(`Component ${WrappedComponent.name} mounted`);
-    }
-
-    render() {
-      return <WrappedComponent {...this.props} />;
-    }
-  };
+    // <React.Fragment>
+    //   <Props />
+    // </React.Fragment>
+  );
 };
-
-// Regular component
-const MyComponent = (props) => {
-  return <div>Hello, {props.name}!</div>;
-};
-
-// Enhanced component with logging
-const EnhancedComponent = withLogging(MyComponent);
-
-// Usage
-<EnhancedComponent name="John" />;
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<withLogging/>);
+root.render(<App/>);
